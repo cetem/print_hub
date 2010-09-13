caa1f9827b7b738b5fbdc6c9effea0fb64dd236e
Franco Catena <francocatena@gmail.com>
2010-09-13 15:48:26 -0300

    Volcado inicial del proyecto

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PrintHub::Application.config.secret_token = 'e0fe2fb5de0889b8ac8b981e69e7a454db33f14ec3b08c195548f4bf4e013ed090d8011fa9017dc4f22e8a3f987d73205d5c840eaacf8b3d740dc9fa832d7946'
