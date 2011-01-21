c6e3336028107896477f336dcc22f475cda3bac3
Franco Catena <francocatena@gmail.com>
2011-01-21 16:05:24 -0300

    Mejoras en la visualización de documentos
    Ahora se muestra la miñatura en la vista de documentos

--- app/helpers/documents_helper.rb
+++ app/helpers/documents_helper.rb
@@ -1,2 +1,11 @@
 module DocumentsHelper
+  def show_document_thumb(document, style = :pdf_mini_thumb)
+    if document.file.file? && File.exists?(document.file.path(style))
+      dimensions = Paperclip::Geometry.from_file(document.file.path(style))
+      image_tag = image_tag(document.file.url(style), :alt => document.name,
+        :size => dimensions.to_s)
+
+      content_tag :div, image_tag, :class => :image_container
+    end
+  end
 end
