f680fd856779ac99984573f97bbd57446411cccc
Franco Catena <francocatena@gmail.com>
2011-03-18 01:56:07 -0300

    Cambio de nomenclatura en los tamaños de página

--- db/migrate/20110312220045_add_media_to_documents.rb
+++ db/migrate/20110312220045_add_media_to_documents.rb
@@ -2,7 +2,7 @@ class AddMediaToDocuments < ActiveRecord::Migration
   def self.up
     add_column :documents, :media, :string
 
-    Document.all.each { |d| d.update_attributes!(:media => 'A4') }
+    Document.all.each { |d| d.update_attributes!(:media => 'iso_a4_210x297mm') }
   end
 
   def self.down
