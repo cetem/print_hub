156f97d3a40520331498fc3aa4e842d4d7374019
Franco Catena <francocatena@gmail.com>
2011-03-30 23:05:21 -0300

    Mejoras en la vista de impresiones

--- app/helpers/prints_helper.rb
+++ app/helpers/prints_helper.rb
@@ -7,7 +7,8 @@ module PrintsHelper
   def link_to_cancel_print_job(print_job)
     button = button_to(t(:cancel_job, :scope => [:view, :prints]),
       cancel_job_print_path(print_job), :method => :put, :remote => true,
-      :disable_with => t(:disabled_cancel_job, :scope => [:view, :prints]))
+      :disable_with => t(:disabled_cancel_job, :scope => [:view, :prints]),
+      :disabled => !print_job.pending?)
 
     content_tag :div, button, :id => "cancel_print_job_#{print_job.id}",
       :class => :button_form_container
