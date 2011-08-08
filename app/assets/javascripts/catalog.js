74c7ff760c811ff3c2db2ed01b3f9730bb0ebef5
Franco Catena <francocatena@gmail.com>
2011-08-08 00:30:23 -0300

    Mejoras en las mejoras del JS de catálogo =)

--- app/assets/javascripts/catalog.js
+++ app/assets/javascripts/catalog.js
@@ -1,5 +1,7 @@
 jQuery(function() {
   $('a.add_to_order, a.remove_from_order').live('click', function() {
-    $(this).die().html('&nbsp;');
+    $(this).html('#').attr('href', '#').click(function(event) {
+      event.preventDefault();
+    }).removeAttr('data-remote').removeAttr('data-method').css('opacity', '.4');
   });
 });
