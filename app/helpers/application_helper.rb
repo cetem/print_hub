9cb5b1ea749cefbfde7b8bbb30758d6b0c15728a
Franco Catena <francocatena@gmail.com>
2011-09-06 23:03:31 -0300

    Orden de columnas en documentos

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -129,4 +129,13 @@ module ApplicationHelper
     
     form.text_field attribute, options
   end
+  
+  def sortable(column, title = nil)
+    title ||= column.titleize
+    css_class = column == sort_column ? "current #{sort_direction}" : nil
+    direction = column == sort_column && sort_direction == 'asc' ? 'desc' : 'asc'
+    
+    link_to title, {sort: column, direction: direction, page: params[:page]},
+      {:class => css_class}
+  end
 end
