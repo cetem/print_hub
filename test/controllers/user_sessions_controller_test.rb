fee1e28228f76e191f74871e8ccc06c9988c45b0
Marcos Escobar <hexagrarm.me@gmail.com>
2013-11-09 01:04:20 -0300

    1 Falla: No message given en linea 63

--- test/controllers/user_sessions_controller_test.rb
+++ test/controllers/user_sessions_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class UserSessionsControllerTest < ActionController::TestCase
   setup do
-    @user = users(:operator)
+    @operator = users(:operator)
   end
 
   test 'should get new' do
@@ -14,59 +14,60 @@ class UserSessionsControllerTest < ActionController::TestCase
   end
 
   test 'should create user session' do
-    assert_difference '@user.shifts.count' do
+    @operator.close_pending_shifts!
+
+    assert_difference '@operator.shifts.count' do
       post :create, user_session: {
-        username: @user.username, 
-        password: "#{@user.username}123"
+        username: @operator.username, 
+        password: "#{@operator.username}123"
       }
     end
 
     assert user_session = UserSession.find
-    assert_equal @user, user_session.user
+    assert_equal @operator, user_session.user
     assert_redirected_to prints_url
   end
   
   test 'should create user session with pending shift' do
-    @user.start_shift!
+    @operator.start_shift!
 
     assert !session[:has_an_open_shift]
     
-    assert_no_difference '@user.shifts.count' do
+    assert_no_difference '@operator.shifts.count' do
       post :create, user_session: {
-        username: @user.username, 
-        password: "#{@user.username}123"
+        username: @operator.username, 
+        password: "#{@operator.username}123"
       }
     end
 
     assert !session[:has_an_open_shift]
 
     assert user_session = UserSession.find
-    assert_equal @user, user_session.user
+    assert_equal @operator, user_session.user
     assert_redirected_to prints_url
   end
   
   test 'should create user session with stale shift' do
-    user = users(:operator)
-
+   
     assert !session[:has_an_open_shift]
     
-    assert_no_difference '@user.shifts.count' do
+    assert_no_difference '@operator.shifts.count' do
       post :create, user_session: {
-        username: user.username,
-        password: "#{@user.username}123"
+        username: @operator.username,
+        password: "#{@operator.username}123"
       }
     end
 
     assert user_session = UserSession.find
-    assert_equal user, user_session.user
+    assert_equal @operator, user_session.user
     assert session[:has_an_open_shift]
     assert_redirected_to edit_shift_url(shifts(:open_shift))
   end
   
   test 'should not create a user session' do
-    assert_no_difference '@user.shifts.count' do
+    assert_no_difference '@operator.shifts.count' do
       post :create, user_session: {
-        username: @user.username,
+        username: @operator.username,
         password: 'wrong'
       }
     end
@@ -81,11 +82,10 @@ class UserSessionsControllerTest < ActionController::TestCase
   test 'should not create a user session with a disabled user' do
     disabled_user = users(:operator)
     disabled_user.update_attributes(enable: false)
-    
     assert_no_difference 'disabled_user.shifts.count' do
       post :create, user_session: {
         username: disabled_user.username, 
-        password: "#{@user.username}123"
+        password: "#{@operator.username}123"
       }
     end
 
@@ -97,31 +97,33 @@ class UserSessionsControllerTest < ActionController::TestCase
   end
 
   test 'should destroy user session and close shift' do
-    assert_difference '@user.shifts.count' do
-      UserSession.create(@user)
+    @operator.close_pending_shifts!
+  
+    assert_difference '@operator.shifts.count' do
+      UserSession.create(@operator)
     end
 
     assert_not_nil UserSession.find
     
-    assert_equal 1, @user.shifts.pending.size
+    assert_equal 1, @operator.shifts.pending.size
     
     delete :destroy, close_shift: true
     
-    assert_equal 0, @user.shifts.pending.reload.size
+    assert_equal 0, @operator.shifts.pending.reload.size
 
     assert_nil UserSession.find
     assert_redirected_to new_user_session_url
   end
   
   test 'should exit whitout close the shift' do
-    UserSession.create(@user)
+    UserSession.create(@operator)
     
-    assert_equal 1, @user.shifts.pending.size
+    assert_equal 1, @operator.shifts.pending.size
     
     delete :destroy
     
-    assert_equal 1, @user.reload.shifts.pending.size
-    assert_nil @user.shifts.pending.last.finish
+    assert_equal 1, @operator.reload.shifts.pending.size
+    assert_nil @operator.shifts.pending.last.finish
     
     assert_nil UserSession.find
     assert_redirected_to new_user_session_url
