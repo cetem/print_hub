999fc6742ef6ff35aef49eb74f7c5dd50a25413e
Néstor Coppi <nestorcoppi@gmail.com>
2014-02-14 22:05:05 -0300

    Volviendo al viejo triple-Rol

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -1,5 +1,8 @@
 set :stage, :production
+set :rails_env, 'production'
 
-role :all, %w{fotocopia.frm.utn.edu.ar}
+role :web, %w{fotocopia.frm.utn.edu.ar}
+role :app, %w{fotocopia.frm.utn.edu.ar}
+role :db, %w{fotocopia.frm.utn.edu.ar}
 
 server 'fotocopia.frm.utn.edu.ar', user: 'deployer', roles: %w{web app db}
--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -1,9 +1,12 @@
 set :stage, :staging
+set :rails_env, 'sandbox'
 set :ssh_options, { port: 26 }
 set :rbenv_type, :user
 set :rbenv_ruby, '2.0.0-p247'
 set :user, 'rotsen'
 
-role :all, %w{192.168.0.8}
+role :web, %w{192.168.0.8}
+role :app, %w{192.168.0.8}
+role :db, %w{192.168.0.8}
 
 server '192.168.0.8', user: 'rotsen', roles: %w{web app db}
