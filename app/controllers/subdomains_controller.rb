7fc1aed91f7238ad8ef3279a70f5732d00348e3f
Néstor Coppi <nestorcoppi@gmail.com>
2014-06-07 17:38:12 -0300

    El root a nueva impresión

--- app/controllers/subdomains_controller.rb
+++ app/controllers/subdomains_controller.rb
@@ -3,7 +3,7 @@ class SubdomainsController < ApplicationController
     url = case request.subdomains.first
       when APP_CONFIG['subdomains']['customers'] then new_customer_session_path
       else
-        new_user_session_path
+        new_print_path
       end
 
     redirect_to url
