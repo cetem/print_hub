bfa8d2537bc249315f3b9d60d807f8e6c56ad26f
Néstor Coppi <nestorcoppi@gmail.com>
2016-01-28 11:04:14 -0300

    Little api script

--- app/helpers/shift_closures_helper.rb
+++ app/helpers/shift_closures_helper.rb
@@ -1,7 +1,5 @@
 module ShiftClosuresHelper
   def printer_has_counter_script?(printer)
-    PrintersApi.available_printers.any? do |pattern|
-      printer.match(pattern)
-    end
+    PrintersApi.has_counter_script?(printer)
   end
 end
--- lib/printers_api.rb
+++ lib/printers_api.rb
@@ -8,8 +8,34 @@ module PrintersApi
       ]
     end
 
+    def has_counter_script?(printer)
+      available_printers.any? { |pattern| printer.match(pattern) }
+    end
+
     def get_counter_for(printer_name)
-      Rails.env.development? ? rand(99999) : nil
+      return unless has_counter_script?(printer_name)
+
+      case
+        when printer_name.match(/ricoh/)
+          ricoh_web_monitor_for_ip(
+            get_printer_ip(printer_name)
+          )
+      end
+    end
+
+    def get_printer_ip(printer)
+      Cups.options_for(printer)['device-uri'].match(/(\d{,3}\.\d{,3}\.\d{,3}\.\d{,3})/)[1]
+    rescue
+      nil
+    end
+
+    def ricoh_web_monitor_for_ip(ip)
+      return if ip.blank?
+      page = open("http://#{ip}/web/guest/es/websys/status/getUnificationCounter.cgi")
+      parsed = Nokogiri::HTML(page)
+      parsed.css('tr.staticProp:contains("Total"):first').children[3].text.to_i
+    rescue
+      nil
     end
   end
 end
