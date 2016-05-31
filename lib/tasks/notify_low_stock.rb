f0aa09f51187d12e5cbc63e867dcdf7fadee372d
Néstor Coppi <nestorcoppi@gmail.com>
2016-05-30 23:56:39 -0300

    Ups

---
+++ lib/tasks/notify_low_stock.rb
@@ -0,0 +1,41 @@
+namespace :tasks do
+  desc 'Notify low articles stock'
+  task notify_low_stock: :environment do
+    articles = Article.to_notify
+
+    if articles.count > 0
+      text = I18n.t(
+				'view.articles.notification_body',
+				body: articles.map(&:notification_message).join("\n")
+      )
+      send_notification(text)
+    end
+  end
+
+  private
+    def logger
+      return @_logger if @_logger
+      @_logger = TasksLogger
+      @_logger.progname = 'NotifyLowStock'
+      @_logger
+    end
+
+    def log_error(ex)
+      error = "#{ex.class}: #{ex.message}\n"
+      error << ex.backtrace.join("\n")
+      logger.error(error)
+    end
+
+    def send_notification(text)
+			token = SECRETS[:telegram][:token]
+			chat_id = SECRETS[:telegram][:chat_id]
+			url = "https://api.telegram.org/bot#{token}/sendMessage"
+			params = { chat_id: chat_id, text: text }.to_param
+
+			if open(url + '?' + params).status.include?('200')
+        logger.info 'Notificación enviada'
+      end
+    rescue => e
+      log_error(e)
+    end
+end
