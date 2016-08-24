31ac5d29ecc88611faef23ede144d3b7be08981c
Néstor Coppi <nestorcoppi@gmail.com>
2016-08-24 00:59:37 -0300

    Trying with sinatra auth

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,1 +1,4 @@
 Sidekiq.default_worker_options = { 'retry' => 2 }
+
+Sidekiq::Web.set :session_secret, Rails.application.secrets[:secret_token]
+Sidekiq::Web.set :sessions, Rails.application.config.session_options
