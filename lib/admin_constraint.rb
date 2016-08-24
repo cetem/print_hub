7656467bf2801b5dac0df3a9a540b23fe8f10472
Néstor Coppi <nestorcoppi@gmail.com>
2016-08-24 01:17:17 -0300

    AdminContraint

--- lib/admin_constraint.rb
+++ lib/admin_constraint.rb
@@ -1,8 +1,11 @@
 class AdminConstraint
   def matches?(request)
-    Rails.logger.info("Someone wants to loggin #{request.cookie_jar['user_credentials']}")
-    return false unless request.cookie_jar['user_credentials'].present?
-    user = User.find_by_persistence_token(request.cookie_jar['user_credentials'].split(':')[0])
-    user && user.admin? && user.not_shifted
+    credentials = request.cookie_jar['user_credentials']
+    Rails.logger.info("Someone wants to loggin #{credentials}")
+    return false if credentials.blank?
+    user = User.find_by_persistence_token(credentials.split(':')[0])
+    will_loggin = user && user.admin? && user.not_shifted
+    Rails.logger.info("User: #{user.to_s} request (#{will_loggin})")
+    will_loggin
   end
 end
