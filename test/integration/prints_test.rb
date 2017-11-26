5df89add9a90c5831654dd7451c1de5341e50b33
Néstor Coppi <nestorcoppi@gmail.com>
2017-11-26 18:23:00 -0300

    Travis prompt

--- test/integration/prints_test.rb
+++ test/integration/prints_test.rb
@@ -4,6 +4,9 @@ class PrintsTest < ActionDispatch::IntegrationTest
   setup do
     @ac_field = 'auto-document-print_job_print_print_jobs_attributes_'
     @pdf_printer = Cups.show_destinations.detect { |p| p =~ /pdf/i }
+    if ENV['TRAVIS']
+      puts Cups.show_destinations
+    end
   end
 
   test 'should add a document with +' do
