62e8eb7fa3b759c923046a66e0f57b2ac6ca91f6
Néstor Coppi <nestorcoppi@gmail.com>
2018-02-19 23:07:18 -0300

    Unicorn restart

--- config/deploy.rb
+++ config/deploy.rb
@@ -24,6 +24,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app) do
+      execute "kill -USR2 $(ps aux |grep unicorn |grep master |awk '{print $2}' )"
       # execute :service, :unicorn, :upgrade
     end
   end
