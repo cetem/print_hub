d9bf664750a2cc5f1de8e7883e0423f4ef46c17d
Néstor Coppi <nestorcoppi@gmail.com>
2019-12-04 18:23:50 -0300

    Save withdraws to be approved in Abaco

--- app/models/withdraw.rb
+++ app/models/withdraw.rb
@@ -1,5 +1,18 @@
 class Withdraw < ActiveRecord::Base
-
   belongs_to :shift_closure
   belongs_to :user
+
+  after_create :save_in_redis
+
+  def save_in_redis
+    ::Redis.new.hset(
+      'shared:pending_withdraws',
+      id,
+      {
+        user:         user.to_s,
+        amount:       amount,
+        collected_at: collected_at.to_i
+      }.to_json
+    )
+  end
 end
