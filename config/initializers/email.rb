94787d10c321fd0f4ab0db2a5942958c959f4ed9
Néstor Coppi <nestorcoppi@gmail.com>
2020-04-29 13:57:40 -0300

    Ups SMTP for production

--- config/initializers/email.rb
+++ config/initializers/email.rb
@@ -2,7 +2,7 @@ Rails.application.routes.default_url_options ||= {}
 Rails.application.routes.default_url_options[:host] = APP_CONFIG['public_host']
 Rails.application.configure do
   config.action_mailer.default_url_options ||= {}
-  config.action_mailer.delivery_method = Rails.env.test? ? :test : :test
+  config.action_mailer.delivery_method = Rails.env.test? ? :test : :smtp
   config.action_mailer.default_url_options[:host] = APP_CONFIG['public_host']
   config.action_mailer.smtp_settings = APP_CONFIG['smtp'].symbolize_keys
 end
